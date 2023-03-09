df82b16d9442ff3ce4907e898cea5729b06983dc
nguyenlppd07982 <nguyenlppd07982@fpt.edu.vn>
2023-03-09 14:30:59 +0700

    nop lab1

---
+++ Lab1/app.js
@@ -0,0 +1,14 @@
+// Nhập a từ bàn phím
+let a = prompt("Nhập số nguyên a:")
+
+// Nhập b từ bàn phím
+let b = prompt("Nhập số nguyên b:")
+
+// Gắn c bằng a + b và ép kiểu a và b về số nguyên
+let c = parseInt(a) + parseInt(b)
+
+// In ra màn hình kết quả c
+document.write("Kết quả a + b = ",c)
+
+// Console.log ra kết quả c
+console.log("Kết quả a + b = "+ c)
