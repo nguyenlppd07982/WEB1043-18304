bda909a79945ec30342fcc7035bd338ed74715bb
nguyenlppd07982 <nguyenlppd07982@fpt.edu.vn>
2023-03-31 16:56:46 +0700

    ass2

--- Assignment1/main.js
+++ Assignment1/main.js
@@ -1,16 +1,25 @@
+// console.log(monthNow);
+// console.log(dayNow);
+// console.log(yearNow);
+
+
+
+
+
+var timeSale = new Date().getTime() + 3 * 60 * 60 * 1000
+
 function ShowTime() {
     var hourse = document.querySelector('.hourse')
     var minutes = document.querySelector('.minutes')
-    var seconds = document.querySelector('.seconds')
-    var date = new Date()
-    var monthNow = date.getMonth()+1
-    var dayNow = date.getDate()
-    var yearNow = date.getFullYear()
-    var timeSale = new Date(`${monthNow} ${dayNow+1} ${yearNow} 00:00:00`)
-    var sale = timeSale-date
-    var hourseSale = Math.ceil(sale / 1000 /60 /60);
-    var minutesSale = Math.ceil(sale / 1000 /60 %60);
-    var secondsSale = Math.ceil(sale / 1000 %60);
+    var seconds = document.querySelector('.seconds') 
+    var date = new Date().getTime();
+    var sale = Math.ceil(timeSale-date)
+    var hourseSale = Math.floor(sale / 1000 /60 /60);
+    var minutesSale = Math.floor(sale / 1000 /60 % 60);
+    var secondsSale = Math.floor(sale / 1000 %60);
+    if(hourseSale < 10 ){
+        hourseSale = `0${hourseSale}`
+    }
     hourse.innerText = hourseSale
     if(minutesSale < 10 ){
         minutesSale = `0${minutesSale}`
@@ -20,6 +29,13 @@ function ShowTime() {
         secondsSale = `0${secondsSale}`
     } 
     seconds.innerText = secondsSale
+
+    if (sale < 0) {
+        hourse.innerHTML = '00'
+        minutes.innerHTML = '00'
+        seconds.innerHTML = '00'
+        
+      }
 }
     setInterval(ShowTime,1000)
 var displayProducts1 = document.querySelector('.products_page1')
@@ -58,7 +74,6 @@ function shows3() {
     activeProducs2.classList.remove("avtive")
     activeProducs1.classList.remove("avtive")
 }
-
 let slideIndex = 1;
 showSlides(slideIndex);
 function plusSlides(n) {
@@ -70,20 +85,21 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlide");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length){
+    slideIndex = 1
+}
+  if (n < 1){
+    slideIndex = slides.length
+}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-
   slides[slideIndex-1].style.display = "block";
 }
-
 function showNotification(){
     // Lấy value SDT để kt đúng định dạng hay kh
     var lengthPhone = document.querySelector('.formPhone').value;
     var valueEmail = document.querySelector('.formMail').value;
-    
     var notification = document.querySelector('.notification');
     var notificationMail = document.querySelector('.notificationMail');
     var notificationPhone = document.querySelector('.notificationPhone');
@@ -110,16 +126,42 @@ function showNotification(){
     }
     if(notiPhone == true && notiMail == true){
         notification.style.display = "block";
+     document.querySelector('.formPhone').value = "";
+     document.querySelector('.formMail').value = "";
+     document.querySelector('.name_input').value = "";
+     document.querySelector('.textarea').value = "";
+     document.querySelector('.local_input').value = "";
     }
-
 }
 var count_cart = document.querySelector(".add_cart")
 var number_products = document.querySelector('.number_products')
 var count = 0;
-
 function add_cart(){
     count++;
     number_products.innerHTML = count;
     console.log(count)
 }
-// count_cart.addEventListener("click",add_cart)
+var element = document.getElementsByClassName("heart_products");
+    for (let i = 0; i < element.length; i++) {
+        element[i].onclick = function() {
+        console.log(i);
+        var btn_heart = document.querySelectorAll(".heart_test")
+        btn_heart[i].classList.toggle("heart_red")
+        console.log(btn_heart);
+    };
+}
+
+var btn_login = document.querySelector('.btn_login')
+btn_login.addEventListener('click', function(){
+    document.querySelector('.login-container').classList.toggle('hiden_form')
+})
+
+var fa_xmark = document.querySelector('.fa-xmark')
+fa_xmark.addEventListener('click', function(){
+    document.querySelector('.login-container').classList.toggle('hiden_form')
+})
+var login_container = document.querySelector('.login-container')
+login_container.addEventListener('click', function(){
+})
+
+
